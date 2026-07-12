6a1200a3b615ae084e2264263b8f751982118289
Asir003 <asirhamim03@gmail.com>
2026-07-12 13:04:03 +0600

    add the data set and set its path in the config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 # Project paths
-
+PROJECT_ROOT: Path = Path(__file__).resolve().parent
+DATASET_PATH: Path = PROJECT_ROOT / "dataset" / "ai4i2020.csv"
 # Dataset columns
 
 # Training hyperparameters
